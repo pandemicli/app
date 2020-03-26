fac2c990d9eafcb5be160e63c7f390547456f9c6
Ali Zahid <ali.zahid@live.com>
2020-03-26 13:21:11 +0400

    Android fixes

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,15 @@
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { Platform } from 'react-native'
 import { API_URI } from 'react-native-dotenv'
 
 import { storage } from '../lib'
+
+const uri = __DEV__
+  ? Platform.select({
+      android: API_URI.replace('localhost', '10.0.2.2'),
+      ios: API_URI
+    })
+  : API_URI
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -16,5 +24,5 @@ export const client = new ApolloClient({
       })
     }
   },
-  uri: API_URI
+  uri
 })
