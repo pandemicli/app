9f63d8855e5ac09e983edcacde6ad185b7ac67d9
Ali Zahid <ali.zahid@live.com>
2020-03-31 21:19:37 +0400

    Key updated

--- src/scenes/auth/landing.tsx
+++ src/scenes/auth/landing.tsx
@@ -22,7 +22,7 @@ export const Landing: FunctionComponent<Props> = ({
     <View style={styles.main}>
       <Image source={img_pandemicli} style={styles.logo} />
       <Text style={styles.title}>{i18n.t('pandemicli')}</Text>
-      <Text style={styles.description}>{i18n.t('landing__message')}</Text>
+      <Text style={styles.description}>{i18n.t('auth__landing__message')}</Text>
       <Carousel style={styles.carousel}>
         <View style={styles.card}>
           <Text style={styles.subtitle}>
